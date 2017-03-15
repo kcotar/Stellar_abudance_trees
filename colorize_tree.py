52106bebc2090abf22be0c46ff8588a08a9dff20
kcotar <cotar.klemen@gmail.com>
2017-03-15 09:31:01 +0100

    Updated ploting
    - known clusters investigation
    - plotting cluster memebers
    - ploting feh, logg, teff
    - code cleanup

---
+++ colorize_tree.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+from ete3 import Tree, TreeStyle, NodeStyle, CircleFace, faces
+from colour import Color
+
+
+def mark_objects(tree, targets, path='tree.png'):
+    # create a copy of three that will be modified later on
+    tree_copy = tree.copy('newick')
+    # define tree plot style
+    tree_plot = TreeStyle()
+    tree_plot.show_leaf_name = False
+    tree_plot.show_branch_length = False
+    tree_plot.show_branch_support = False
+    tree_plot.mode = 'c'
+    # also count number of marks
+    n_marks = 0
+    for branch in tree_copy.traverse():
+        if branch.is_leaf():
+            branch_sobject_id = np.int64(branch.name)
+            if branch_sobject_id in targets:
+                b_facet = CircleFace(radius=10, color='red', style='circle')
+                branch.add_face(b_facet, 0, position='branch-right')
+                n_marks += 1
+            else:
+                b_style = NodeStyle()
+                b_style['size'] = 0
+                branch.set_style(b_style)
+        else:
+            b_style = NodeStyle()
+            b_style['size'] = 0
+            branch.set_style(b_style)
+    if n_marks > 0:
+        tree_copy.render(path, h=8000, w=8000, units='px', tree_style=tree_plot)
+
+
+def colorize_tree(tree, dataset, feature, path='tree.png'):
+    # create a copy of three that will be modified later on
+    tree_copy = tree.copy('newick')
+    # define tree plot style
+    tree_plot = TreeStyle()
+    tree_plot.show_leaf_name = False
+    tree_plot.show_branch_length = False
+    tree_plot.show_branch_support = False
+    tree_plot.mode = 'c'
+    # determine colour set for given dataset
+    colorize_dataset = dataset[feature]
+    n_steps = 300
+    color_labels = list(Color('red').range_to(Color('blue'), n_steps))
+    colour_data_min = np.nanpercentile(colorize_dataset, 1.)
+    colour_data_max = np.nanpercentile(colorize_dataset, 99.)
+    colour_data_range = colour_data_min + np.arange(0, n_steps) * (colour_data_max - colour_data_min) / n_steps
+    # print colour_data_min, colour_data_max
+    # print colour_data_range[0], colour_data_range[-1]
+    # iterate over every tree element
+    for branch in tree_copy.traverse():
+        if branch.is_leaf():
+            idx = np.where(dataset['sobject_id'] == np.int64(branch.name))
+            data_val = colorize_dataset[idx]
+            colour_string = str(color_labels[np.nanargmin(np.abs(colour_data_range - data_val))])
+            # node style
+            # b_style = NodeStyle()
+            # b_style['fgcolor'] = colour_string
+            # b_style['size'] = 0
+            # b_style['hz_line_color'] = colour_string
+            # b_style['hz_line_width'] = 40
+            # branch.set_style(b_style)
+            b_facet = CircleFace(radius=10, color=colour_string, style='circle')
+            branch.add_face(b_facet, 0, position='float')
+        else:
+            b_style = NodeStyle()
+            b_style['size'] = 0
+            branch.set_style(b_style)
+    tree_copy.render(path, h=8000, w=8000, units='px', tree_style=tree_plot)
--- test_nj_method.py
+++ test_nj_method.py
@@ -1,36 +1,95 @@
-import os
+import os, imp
 import numpy as np
+import astropy.units as u
+import astropy.coordinates as coord
 
 from sklearn.metrics.pairwise import manhattan_distances
 from skbio import DistanceMatrix
 from skbio.tree import nj
 from astropy.table import Table
 
+imp.load_source('helper', '../tSNE_test/helper_functions.py')
+from helper import move_to_dir, get_abundance_cols, define_cluster_centers
+imp.load_source('norm', '../Stellar_parameters_interpolator/data_normalization.py')
+from norm import *
+
+from colorize_tree import *
+
+from ete3 import Tree
+
+
 galah_data_dir = '/home/klemen/GALAH_data/'
-galah_cannon = Table.read(galah_data_dir+'sobject_iraf_cannon_1.2.fits')[100000:105000]
+nj_data_dir = '/home/klemen/NJ_tree_settings/'
+# Galah parameters and abundance data
+galah_cannon = Table.read(galah_data_dir+'sobject_iraf_cannon_1.2.fits')
+# known stars in clusters in this dataset
+stars_cluster_data = Table.read(galah_data_dir+'sobject_clusterstars_1.0.fits')
+clusters_ra, clusters_dec = define_cluster_centers(stars_cluster_data, galah_cannon)
+abund_cols = get_abundance_cols(galah_cannon.colnames)
 
-abund_cols = [col for col in galah_cannon.colnames if '_abund_' in col and '_e_' not in col]
-# galah_ids = galah_cannon['sobject_id']._void
-galah_cannon_data = np.array(galah_cannon[abund_cols].to_pandas())
-print np.shape(galah_cannon_data)
+# for ra_center, dec_center in zip([],[]):
+#
+# convert ra and dec to astropy coordinates
+ra_center = 57.
+dec_center = 24.
+search_dist = 4.
+ra_dec_coord = coord.ICRS(ra=np.array(galah_cannon['ra'])*u.degree,
+                          dec=np.array(galah_cannon['dec'])*u.degree)
+for ra_center, dec_center in zip(clusters_ra, clusters_dec):
+    if ra_center is np.nan or dec_center is np.nan:
+        continue
+    stars_arc_dist = ra_dec_coord.separation(coord.ICRS(ra=ra_center*u.degree, dec=dec_center*u.degree))
+    stars_in_area = stars_arc_dist < search_dist*u.degree
+    n_stars_in_area = np.sum(stars_in_area)
+    print 'Number of stars found in search location: '+str(n_stars_in_area)
+    if n_stars_in_area < 50:
+        continue
+    # create a subset of the dataset
+    galah_cannon_subset = galah_cannon[stars_in_area]
+    galah_cannon_abund_data_sebset = np.array(galah_cannon_subset[abund_cols].to_pandas())
+    norm_params = normalize_data(galah_cannon_abund_data_sebset, method='standardize')
 
-distances = manhattan_distances(galah_cannon_data)
-# export distances to be used by megacc procedure
-txt = open('distances.meg', 'w')
-txt.write('#mega\n')
-txt.write('!Title Mega file;\n')
-txt.write('!Description Test file;\n')
-txt.write('!Format DataType=distance DataFormat=lowerleft;\n')
-# export all taxa
-for s_id in galah_cannon['sobject_id']:
-    txt.write('#'+str(s_id)+'\n')
-# output settings
-for i_r in range(1, distances.shape[0]):
-    txt.write(' '.join([str(f) for f in distances[i_r, 0: i_r]])+'\n')
-txt.close()
+    move_to_dir('Stellar_neighbour_tree_ra_{:0.1f}_dec_{:0.1f}_rad_{:0.1f}'.format(ra_center, dec_center, search_dist))
+    output_nwm_file = 'distances_network.nwk'
+    # compute distances and create phylogenetic tree from distance information
+    if not os.path.isfile(output_nwm_file):
+        distances = manhattan_distances(galah_cannon_abund_data_sebset)
+        # export distances to be used by megacc procedure
+        txt = open('distances.meg', 'w')
+        txt.write('#mega\n')
+        txt.write('!Title Mega_distances_file;\n')
+        txt.write('!Description Abundances_test_file;\n')
+        txt.write('!Format DataType=distance DataFormat=lowerleft;\n')
+        # export all taxa
+        for s_id in galah_cannon_subset['sobject_id']:
+            txt.write('#'+str(s_id)+'\n')
+        # output settings
+        for i_r in range(1, distances.shape[0]):
+            txt.write(' '.join(['{:0.4f}'.format(f) for f in distances[i_r, 0: i_r]])+'\n')
+        txt.close()
+        os.system('megacc -a '+nj_data_dir+'infer_NJ_distances.mao -d distances.meg -o '+output_nwm_file)
 
-os.system('megacc -a infer_NJ_distances.mao -d distances.meg -o distances_network.nwk')
+        # - OR -
+        # alternative way to build the tree, much slower option
+        # dm = DistanceMatrix(distances)
+        # nj_tree = nj(dm)
+        # print(nj_tree.ascii_art())
 
-# dm = DistanceMatrix(distances)
-# nj_tree = nj(dm)
-# print(nj_tree.ascii_art())
+    # read output tree file
+    if os.path.isfile(output_nwm_file):
+        txt = open(output_nwm_file, 'r')
+        tree_struct = Tree(file.readline(txt)[:-1])
+        txt.close()
+    else:
+        print 'ERROR: check megacc as it did not produce the following file '+output_nwm_file
+    print 'Plotting graphs'
+    for cluster in np.unique(stars_cluster_data['cluster_name']):
+        cluster_targets = stars_cluster_data[stars_cluster_data['cluster_name'] == cluster]['sobject_id']
+        mark_objects(tree_struct, cluster_targets, path='cluster_'+cluster+'.png')
+    colorize_tree(tree_struct, galah_cannon_subset, 'logg_cannon', path='tree_logg.png')
+    colorize_tree(tree_struct, galah_cannon_subset, 'feh_cannon', path='tree_feh.png')
+    colorize_tree(tree_struct, galah_cannon_subset, 'teff_cannon', path='tree_teff.png')
+    for abund in abund_cols:
+        colorize_tree(tree_struct, galah_cannon_subset, abund, path='tree_abund_'+abund+'.png')
+    os.chdir('..')
+
